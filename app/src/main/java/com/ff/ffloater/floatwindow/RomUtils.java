32b37a0c387c210b7954746eb3be0b94cb84970f
feifan <feifan@bilibili.com>
2018-01-29 17:50:11 +0800

    old implementation may cause ANR

--- app/src/main/java/com/ff/ffloater/floatwindow/RomUtils.java
+++ app/src/main/java/com/ff/ffloater/floatwindow/RomUtils.java
@@ -3,11 +3,7 @@ package com.ff.ffloater.floatwindow;
 import android.os.Build;
 import android.support.annotation.StringDef;
 import android.text.TextUtils;
-import android.util.Log;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -54,26 +50,7 @@ public class RomUtils {
     private static final String SYSTEM_VERSION_LENOVO = "ro.lenovo.lvp.version";
 
     private static String getSystemProperty(String propName) {
-        String line;
-        BufferedReader input = null;
-        try {
-            Process p = Runtime.getRuntime().exec("getprop " + propName);
-            input = new BufferedReader(new InputStreamReader(p.getInputStream()), 1024);
-            line = input.readLine();
-            input.close();
-        } catch (IOException ex) {
-            Log.e(TAG, "Unable to read sysprop " + propName, ex);
-            return null;
-        } finally {
-            if (input != null) {
-                try {
-                    input.close();
-                } catch (IOException e) {
-                    Log.e(TAG, "Exception while closing InputStream", e);
-                }
-            }
-        }
-        return line;
+        return SystemProperties.get(propName, null);
     }
 
     @RomName
